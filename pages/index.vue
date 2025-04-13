1fb304c808467914a7e460c09ca9450fd1d8db03
Dep <70801324+Depreca1ed@users.noreply.github.com>
2025-04-13 13:52:19 +0530

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,17 @@
 <template>
-  <div>
-    <h1>aaaaaaa</h1>
-  </div>
-</template>
+    <div class="card">
+        <div class="card_name">
+            <h1>Hello! I'm Dep</h1>
+        </div>
+        <div class="card_content">
+            <p>I am a cool person who develops shit from time to time.</p>
+            <p>Currently, I am proficient in:</p>
+            <ul>
+                <li>Python</li>
+                <li>Yeah.... thats it.</li>
+            </ul>
+        </div>
+    </div></template>
 
 <script lang="ts" setup>
 </script>
