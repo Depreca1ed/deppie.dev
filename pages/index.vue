784f9d676f5c929d09632a6ce5d6abab304161a3
Depreca1ed <70801324+Depreca1ed@users.noreply.github.com>
2025-07-23 03:33:47 +0530

    Rewrite index

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,41 @@
 <template>
-  <div class="flex flex-col mx-auto">
-    <div class="flex flex-col flex-wrap items-center mt-10 gap-5">
-      <img class="place-self-center self-center size-[25%] rounded-4xl" :src=SOCIALS.primary.avatar>
-      <div class="flex flex-col gap-y-5">
-        <div class="text-center text-2xl font-semibold text-gray-300">
-          <p>Hi, I am </p>
-          <p class="font-bold text-white text-8xl">{{ SOCIALS.primary.name }}</p>
-        </div>
-        <div class="text-center text-2xl font-semibold text-gray-500">
-          <p>(Deprecated)</p>
-        </div>
+<div class="flex flex-col m-[5%] gap-y-10 items-center justify-center h-max">
+  <div class='flex md:flex-row flex-col flex-warp items-center justify-center gap-y-10 w-full'>
+    <img class="flex size-[50%] md:size-[25%] rounded-full outline-2 outline-offset-4 outline-[#cba6f7]/25" :src=userdata.avatar.path>
+    <div class='flex flex-col gap-y-5 items-center'>
+      <p class='font-bold text-5xl'>{{userdata.username}}</p>
+
+      <hr class='flex border border-[#cba6f7]/25 size-auto w-[75%]'>
+
+      <div class='flex flex-row self-center items-center justify-center' v-for="i, social in userdata.socials">
+        <a class="flex self-center items-center justify-center" :href=i>
+          <span class="flex items-center justify-center size-[10%] rounded-full hover:outline-2 outline-offset-4 outline-[#f38ba8]/50">
+            <img class="invert" :src=social :href=i>
+          </span>
+        </a>
       </div>
-    </div>
-    <div class="flex border-t-1">
-      <div class="text-center mx-5 text-2xl text-wrap font-semibold">
-        <p>I love thighs..... A LOT</p>
-      </div>
+
     </div>
   </div>
+  <hr class='flex border border-[#cba6f7]/25 size-auto w-[75%]'>
+  <div class='flex items-center justify-center w-[90%]'>
+    <p class='text-xl'>
+    {{userdata.about}}
+    </p>
+  </div>
+</div>
 </template>
 
-<script lang="ts" setup>
-  const SOCIALS = { 'primary': { 'name': "Dep", 'avatar': "https://avatars.githubusercontent.com/u/70801324" } }
-
+<script lang='ts' setup>
+  const userdata = {
+    "username": "Deppie",
+    "avatar": {
+      "path": "avatar.png"
+    },
+    "socials":{"github.png": "https://github.com/Depreca1ed",},
+    "about": "I love thighs.",
+  };
 </script>
 
 <style>
-
-  html {
-    background: url("/assets/bg.webp");
-    background-size: fill;
-    background-repeat: no-repeat;
-    background-blend-mode: darken;
-    background-color: #000000b0;
-  }
 </style>
